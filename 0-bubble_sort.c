b2b9bbd74f2d6225b2d8a0a6d90024d19970aac1
DannySanchez6658 <danieltsega6658@gmail.com>
2023-04-24 08:45:14 -0700

    [Feature]: Adding the buuble sort function

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - a function that uses
+ * a bubble sort method to sort an array.
+ *
+ * @array: a pointer to an array
+ * @size: a size of the array
+ * Return: nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+
+	int temp;
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+
+		for (j = 0; j < size - i - 1; j++)
+		{
+
+			if (array[j] > array[j + 1])
+			{
+
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
